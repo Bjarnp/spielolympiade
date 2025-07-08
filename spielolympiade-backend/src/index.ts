882639165f9b38598d246cc52e9d4c854c203012
Bjarne Kruse <Bjarne.Kruse@edu.siemens-energy.com>
2025-07-08 20:42:48 +0200

    add cors

--- spielolympiade-backend/src/index.ts
+++ spielolympiade-backend/src/index.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import authRoutes from "./routes/auth";
 import userRoutes from "./routes/users";
@@ -11,6 +12,7 @@ import { authenticateToken } from "./middleware/auth";
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
