a12be013bc31be631e31e6dd8cf02f14a23add44
Bjarne Kruse <Bjarne.Kruse@edu.siemens-energy.com>
2025-07-09 20:12:43 +0200

    ngif import

--- spielolympiade-frontend/src/app/app.component.ts
+++ spielolympiade-frontend/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { AuthService } from './core/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgIf],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'spielolympiade-frontend';
