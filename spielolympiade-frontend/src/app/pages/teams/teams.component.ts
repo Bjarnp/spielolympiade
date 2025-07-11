e42a042b02a990de1e8523061626944b834fb9be
Bjarne <62839459+Bjarnp@users.noreply.github.com>
2025-07-12 00:43:41 +0200

    Add team overview page

--- spielolympiade-frontend/src/app/pages/teams/teams.component.ts
+++ spielolympiade-frontend/src/app/pages/teams/teams.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { environment } from '../../../environments/environment';
+
+const API_URL = environment.apiUrl;
 
 @Component({
   selector: 'app-teams',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatListModule],
   templateUrl: './teams.component.html',
-  styleUrl: './teams.component.scss'
+  styleUrls: ['./teams.component.scss'],
 })
 export class TeamsComponent {
+  http = inject(HttpClient);
 
+  teams: any[] = [];
+
+  ngOnInit(): void {
+    this.http.get<any[]>(`${API_URL}/teams`).subscribe((data) => {
+      this.teams = data;
+    });
+  }
+
+  getMemberNames(team: any): string {
+    return team.members.map((m: any) => m.user.name).join(', ');
+  }
 }
